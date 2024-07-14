367613f917c06dbc4e6af8067f162c8e3933ff73
KATRA50 <naimamohamud4398@gmail.com>
2024-07-14 01:34:17 +0000

    deploy

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return 'Hello Universe!'
-
+    return "Hello, World!"
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
     
